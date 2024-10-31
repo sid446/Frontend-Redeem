861ddc23d1e38e0d7bb01892af861085e30fa0fd
sid446 <162982475+sid446@users.noreply.github.com>
2024-10-31 16:12:42 +0530

    fixing

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  
   plugins: [react()],
   server: {
     port: 5000,
   },
-  build: {
-    outDir: 'dist',
-  }
+  
 })
