607f2d7019622d4cebe18d224354f84b7e07b660
sid446 <162982475+sid446@users.noreply.github.com>
2024-10-31 15:56:50 +0530

    fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-import React from 'react'
-import { BrowserRouter,Route,Router, Routes } from 'react-router-dom'
-import CouponPage from './pages/CouponPage.jsx'
-import QrCheckPage from './pages/QrCheckPage.jsx'
-
-
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import CouponPage from './pages/CouponPage.jsx';
+import QrCheckPage from './pages/QrCheckPage.jsx';
 
 function App() {
   return (
     <BrowserRouter>
-    
-    <Routes>
-      <Route path="/" element={<QrCheckPage/>} />
-      <Route path="/access" element={<QrCheckPage/>} />
-      <Route path="/coupon" element={<CouponPage/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<QrCheckPage />} />
+        <Route path="/access" element={<QrCheckPage />} />
+        <Route path="/coupon" element={<CouponPage />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
