d213b06db8b2bf2966055f410872907cfa5e36f5
sid446 <162982475+sid446@users.noreply.github.com>
2024-10-31 16:21:40 +0530

    fixing

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  
   plugins: [react()],
   server: {
-    port: 5000,
+    historyApiFallback: true, // This line enables client-side routing
   },
-  
-})
+});
