673305d47768f82e8c736ea19c25820c9a0a8d86
sid446 <162982475+sid446@users.noreply.github.com>
2024-11-01 13:10:46 +0530

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,13 @@ import React from 'react'
 import { BrowserRouter,Route,Router, Routes } from 'react-router-dom'
 import CouponPage from './pages/CouponPage.jsx'
 import QrCheckPage from './pages/QrCheckPage.jsx'
+import LoadingPage from './pages/Loading.jsx'
 
 
 
 function App() {
   return (
-    <BrowserRouter>
-    
-    <Routes>
-      <Route path="/" element={<QrCheckPage/>} />
-      <Route path="/access" element={<QrCheckPage/>} />
-      <Route path="/coupon" element={<CouponPage/>}/>
-    </Routes>
-    </BrowserRouter>
+    <LoadingPage/>
   )
 }
 
--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -1,10 +1,10 @@
-
 import React from 'react';
 
 const LoadingPage = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-16 h-16 border-4 border-blue-500 border-t-transparent border-solid rounded-full animate-spin"></div>
+    <div className="flex items-center justify-center min-h-screen bg-[#A9C4D3] relative overflow-hidden">
+      {/* Loading Spinner */}
+      <div className="z-10 w-16 h-16 border-4 border-[#F7D773] border-t-transparent border-solid rounded-full animate-spin"></div>
     </div>
   );
 };
